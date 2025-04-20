d8800fa8b1398fdbff280423dd8ad6684ecf99d9
HoangTrongTan <hoangtrongtan15112000@gmail.com>
2025-04-20 23:59:29 +0700

    add loader for page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,37 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, signal } from '@angular/core';
+import { Event, NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
-import translationsEN from "../../public/i18n/en.json";
-import { NgxImageZoomModule } from 'ngx-image-zoom';
+// import translationsEN from '../../public/i18n/en.json';
+// import { NgxImageZoomModule } from 'ngx-image-zoom';
+import { filter } from 'rxjs';
+import { LoaderComponent } from './components/loader';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoaderComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  constructor(private translate: TranslateService) {
+export class AppComponent implements OnInit {
+  isLoading = signal<boolean>(false);
+
+  constructor(private translate: TranslateService, private router: Router) {
     // translate.setTranslation('en', translationsEN);
     // translate.setDefaultLang('en');
-}
+  }
+  ngOnInit(): void {
+    this.router.events
+      .pipe(
+        filter(
+          (event: Event): event is NavigationEnd =>
+            event instanceof NavigationEnd
+        )
+      )
+      .subscribe((event) => {
+        this.isLoading.set(true);
+        setTimeout(() => {
+          this.isLoading.set(false);
+        }, 1300);
+      });
+  }
 }
