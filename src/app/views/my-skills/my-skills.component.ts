ad2382d2781b2fc97ebb0b84821c1839cc023cc2
HoangTrongTan <hoangtrongtan15112000@gmail.com>
2025-04-28 08:12:05 +0700

    update mySkill page

--- src/app/views/my-skills/my-skills.component.ts
+++ src/app/views/my-skills/my-skills.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
+import Skills from '../../data/my-skill/skill.json';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-my-skills',
-  imports: [MatExpansionModule],
+  imports: [MatExpansionModule, MatProgressBarModule],
   templateUrl: './my-skills.component.html',
   styleUrl: './my-skills.component.scss',
 })
 export class MySkillsComponent {
-  
+  skills = Skills;
+  constructor(){
+    
+  }
 }
